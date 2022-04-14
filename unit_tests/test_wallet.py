32dcfd998543c671ff3f5ce098fccee28fd65825
Greg Doyle <drgregdoyle@gmail.com>
2022-04-14 16:41:17 -0400

    - Modified Wallet so that if incorrect bit/checksum_bit values are used, they get fixed.  - Added the Wallet.get_seed function. The goal is to never save the seed to the Wallet. So we instantiate and it runs get_seed then uses that to generate the keys and the seed_phrase  - seed_phrase is a class var. for the moment. It's needed for testing.

--- unit_tests/test_wallet.py
+++ unit_tests/test_wallet.py
@@ -2,7 +2,7 @@
 Unit tests for the wallet class
 '''
 
-import io
+import numpy as np
 from wallet import Wallet
 
 
@@ -18,3 +18,40 @@ def test_seed_phrase():
 
     assert wallet_128.get_seed_phrase(seed_128) == phrase_128
     assert wallet_256.get_seed_phrase(seed_256) == phrase_256
+
+
+def test_entropy_checksum_mod11():
+    random_num = 0
+    while random_num % 11 == 0:
+        random_num = np.random.randint(150, 250)
+
+    w1 = Wallet()
+    w2 = Wallet(256, 8)
+    w3 = Wallet(50, 5)
+    w4 = Wallet(128, 8)
+    w5 = Wallet(random_num, 0)
+    w6 = Wallet(165, 0)
+
+    # Default case
+    assert w1.bits == 128
+    assert w1.checksum_bits == 4
+
+    # Case where both bits and checksum are correct
+    assert w2.bits == 256
+    assert w2.checksum_bits == 8
+
+    # Case where bits are invalid, checksum is not
+    assert w3.bits == 128
+    assert w3.checksum_bits == 4
+
+    # Case where bits are valid, checksum is not
+    assert w4.bits == 128
+    assert w4.checksum_bits == 4
+
+    # Tests case where bits are random valid number not congruent to 0 (mod 11) yields correct checksum
+    assert w5.bits == random_num
+    assert w5.checksum_bits == -random_num % 11
+
+    # Tests case where bits = 0 (mod 11). Want a nonzero checksum_bits
+    assert w6.bits == 165
+    assert w6.checksum_bits == 11
--- wallet.py
+++ wallet.py
@@ -7,14 +7,17 @@ import pandas as pd
 
 '''Imports'''
 import secrets
-from hashlib import sha256
+from hashlib import sha256, sha512
 
 '''Wallet Class'''
 
 
 class Wallet:
+    '''Formatting Variables'''
+    MIN_EXP = 7
+    DICT_EXP = 11
 
-    def __init__(self, bits=128, checksum_bits=4):
+    def __init__(self, bits=128, checksum_bits=4, seed=None):
         '''
         We create a deterministic wallet where the seed will be expressed as dictionary words.
         We follow common practice by first generating a random_number, hashing the number and using as checksum,
@@ -22,14 +25,42 @@ class Wallet:
             NB: We 2048 words in the dictionary as 2^11 = 2048.
 
         '''
+        '''Automatically adjust bitlength and checksum if out of bounds'''
+        if bits < pow(2, self.MIN_EXP):
+            bits = pow(2, self.MIN_EXP)
+        if (bits + checksum_bits) % self.DICT_EXP != 0:
+            checksum_bits = -bits % self.DICT_EXP
+        if checksum_bits == 0:
+            checksum_bits = self.DICT_EXP
+
         self.bits = bits
         self.checksum_bits = checksum_bits
 
-        '''Verify entropy + checksum_bits'''
-        try:
-            assert (self.bits + self.checksum_bits) % 11 == 0
-        except AssertionError:
-            return None
+        '''Either create a new seed or recover old wallet'''
+        if seed is None:
+            seed = self.get_seed()
+        else:
+            seed = seed
+
+        '''After instantiation we generate the master keys and the recovery phrase'''
+        '''Only way to recover the seed after instantiation is from seed phrase'''
+        self.master_keys = self.generate_master_keys(seed)
+        self.seed_phrase = self.get_seed_phrase(seed)  # Saving seed_phrase is only for testing.
+
+    def get_seed(self):
+        '''
+        Should be run once during instantiation in order to generate a seed for the Wallet.
+        The seed will be needed to generate the mnemonic seed phrase for the user.
+        The seed will be needed to make the "Master Keys" for the Wallet.
+        The seed will be able to be recovered from the seed_phrase in order to generate the Master Keys again.
+        We NEVER save the seed value to the Wallet class.
+        '''
+
+        seed = 0
+        while seed.bit_length() != self.bits:
+            seed = secrets.randbits(self.bits)
+
+        return seed
 
     def get_seed_phrase(self, seed=None) -> list:
         '''
@@ -88,3 +119,6 @@ class Wallet:
         entropy = index_string[:self.bits]
         seed = int(entropy, 2)
         return seed
+
+    def generate_master_keys(self, seed=None):
+        pass
