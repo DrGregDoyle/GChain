f82ae7776c428e7ab829e0c204bffac5fd9d112b
Greg Doyle <drgregdoyle@gmail.com>
2022-05-05 15:35:25 -0400

    Moved generate transaction to test_block

--- transaction.py
+++ transaction.py
@@ -143,36 +143,3 @@ TESTING
 '''
 from wallet import Wallet
 import numpy as np
-
-
-def generate_transaction():
-    '''
-    We create a random number of inputs and outputs and create a Transaction from this.
-    '''
-    # Create inputs
-    inputs = []
-    input_num = 0
-    while input_num == 0:
-        input_num = np.random.randint(10)
-    for x in range(0, input_num):
-        random_string = ''
-        for r in range(0, np.random.randint(100)):
-            random_string += random.choice(string.ascii_letters)
-        tx_id = sha256(random_string.encode()).hexdigest()
-        tx_index = np.random.randint(100)
-        sig = Wallet().sign_transaction(tx_id)
-        inputs.append(UTXO_INPUT(tx_id, tx_index, sig).raw_utxo)
-
-    # Create outputs
-    outputs = []
-    output_num = 0
-    while output_num == 0:
-        output_num = np.random.randint(10)
-    for y in range(0, output_num):
-        amount = np.random.randint(1000)
-        temp_wallet = Wallet()
-        address = temp_wallet.address
-        utxo_output = UTXO_OUTPUT(amount, address)
-        outputs.append(utxo_output.raw_utxo)
-
-    return Transaction(inputs=inputs, outputs=outputs)
